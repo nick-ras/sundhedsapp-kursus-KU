c755af9bd4fc494c8c552f7e10e6e2b928d833b7
Nick Rasmussen <nickras10@gmail.com>
2024-01-06 12:27:04 +0100

    select tabler

--- server_test_i_python/test.py
+++ server_test_i_python/test.py
@@ -12,9 +12,30 @@ try:
         ssl_disabled=True
     )
 
-    # If the connection is successful, print a success message
+    # Close the database connection
     if cnx.is_connected():
         print("Connection to MySQL database successful")
+
+        # Create a cursor object to interact with the database
+        cursor = cnx.cursor()
+
+        # SQL statement to create the DCRUsers table
+        create_table_sql = """
+        SELECT * FROM DCRUsers;
+				"""
+
+        # Execute the SQL statement to create the table
+        temp = cursor.execute(create_table_sql)
+        rows = cursor.fetchall()
+
+        # Print the retrieved data
+        for row in rows:
+            print(row)
+        # Commit the changes to the database
+        cnx.commit()
+
+        # Close the cursor and database connection
+        cursor.close()
 
     # Close the database connection
     cnx.close()
