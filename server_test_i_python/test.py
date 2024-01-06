3e25d6f38615a4b3e1a5ca9758184d2f50e4e052
Nick Rasmussen <nickras10@gmail.com>
2024-01-06 13:15:14 +0100

    made extra table for drcr graphs, dog er alle columns varchar(255) da jeg ikke var sikker på hvad var integers

--- server_test_i_python/test.py
+++ server_test_i_python/test.py
@@ -2,44 +2,52 @@ import mysql.connector
 from os import environ as env
 
 try:
-    # Define the database connection parameters
-    cnx = mysql.connector.connect(
-        user=env.get("USERNAME"),
-        password=env.get("PASSWORD"),
-        host="cloud-kursus.mysql.database.azure.com",
-        port=3306,
-        database=env.get("DATABASE"),
-        ssl_disabled=True
-    )
+		# Define the database connection parameters
+		cnx = mysql.connector.connect(
+				user=env.get("USERNAME"),
+				password=env.get("PASSWORD"),
+				host="cloud-kursus.mysql.database.azure.com",
+				port=3306,
+				database=env.get("DATABASE"),
+				ssl_disabled=True
+		)
 
-    # Close the database connection
-    if cnx.is_connected():
-        print("Connection to MySQL database successful")
 
-        # Create a cursor object to interact with the database
-        cursor = cnx.cursor()
+		# Close the database connection
+		if cnx.is_connected():
+				print("Connection to MySQL database successful")
 
-        # SQL statement to create the DCRUsers table
-        create_table_sql = """
-        SELECT * FROM DCRUsers;
+				# Create a cursor object to interact with the database
+				cursor = cnx.cursor()
+
+				# SQL statement to create the DCRUsers table
+				
+				# create_table_sql = """
+				# INSERT INTO DCRTable (Graph_id, Simulation_id, Process_instance_name, Description) VALUES ("test", "test", "test", "test");
+				# """
+				# cursor.execute(create_table_sql)
+				cnx.commit()
+				select_statement = """
+				SELECT * FROM DCRTable;
 				"""
 
-        # Execute the SQL statement to create the table
-        temp = cursor.execute(create_table_sql)
-        rows = cursor.fetchall()
+				cursor.execute(select_statement)
+				rows = cursor.fetchall()
+				print("now dcr table")
+				for row in rows:
+						print(row)
+				select_statement2 = """
+				SELECT * FROM DCRUsers;
+				"""
+				cursor.execute(select_statement2)
+				rows = cursor.fetchall()
+				
+				print("now users table")
+				for row in rows:
+						print(row)
+				cursor.close()
 
-        # Print the retrieved data
-        for row in rows:
-            print(row)
-        # Commit the changes to the database
-        cnx.commit()
-
-        # Close the cursor and database connection
-        cursor.close()
-
-    # Close the database connection
-    cnx.close()
+		cnx.close()
 
 except mysql.connector.Error as err:
-    # Handle any errors that occurred during the connection attempt
-    print(f"Error: {err}")
+		print(f"Error: {err}")
